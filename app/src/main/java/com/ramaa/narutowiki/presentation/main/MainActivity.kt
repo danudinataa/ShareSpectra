6adc904a2b0bcd7e6327aa3dc203b9964f1a2b98
Rama Danudinata <2110511096@mahasiswa.upnvj.ac.id>
2023-11-21 15:07:56 +0700

    ADD SYSTEM UI COLOR

--- app/src/main/java/com/ramaa/narutowiki/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ramaa/narutowiki/presentation/main/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramaa.narutowiki.presentation.navgraph.NavGraph
 import com.ramaa.narutowiki.ui.theme.NarutoWikiTheme
 import com.ramaa.narutowiki.viewmodel.MainViewModel
@@ -28,6 +32,16 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             NarutoWikiTheme(dynamicColor = false) {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
                     NavGraph(startDestination = viewModel.startDestination.value)
                 }
